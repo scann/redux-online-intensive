92a815c5b1a37cd53fdf41e553403133cf232fe1
scann <apple.eater01@gmail.com>
2019-01-22 21:03:54 +0200

    SHOW_SELECTED_PHOTO action creator

--- source/basic-redux/bus/components/actions.js
+++ source/basic-redux/bus/components/actions.js
@@ -1,6 +1,15 @@
 //Types
-import { SHOW_NEXT_PHOTO } from './types';
+import { SHOW_NEXT_PHOTO, SHOW_SELECTED_PHOTO } from './types';
 
-export const showNextPhoto = {
-    type: SHOW_NEXT_PHOTO,
+export const showNextPhoto = () => {
+    return {
+        type: SHOW_NEXT_PHOTO,
+    };
+};
+
+export const showSelectedPhoto = (photoIndex) => {
+    return {
+        type:    SHOW_SELECTED_PHOTO,
+        payload: photoIndex,
+    };
 };
