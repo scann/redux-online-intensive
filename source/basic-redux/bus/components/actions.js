5860c5e225b170f00d3bc2fd18010408b1cda036
scann <apple.eater01@gmail.com>
2019-01-23 12:52:52 +0200

    Hometask - new SHOW_PREV_PHOTO action creator

--- source/basic-redux/bus/components/actions.js
+++ source/basic-redux/bus/components/actions.js
@@ -1,9 +1,15 @@
 //Types
-import { SHOW_NEXT_PHOTO, SHOW_SELECTED_PHOTO } from './types';
+import { SHOW_NEXT_PHOTO, SHOW_PREV_PHOTO, SHOW_SELECTED_PHOTO } from './types';
 
 export const showNextPhoto = () => {
     return {
         type: SHOW_NEXT_PHOTO,
+    };
+};
+
+export const showPrevPhoto = () => {
+    return {
+        type: SHOW_PREV_PHOTO,
     };
 };
 
