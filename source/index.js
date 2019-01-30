b107759f7608c48831d33501f20fbe3883e932bc
scann <apple.eater01@gmail.com>
2019-01-30 14:28:27 +0200

    Provider wrapper with store added to App component

--- source/index.js
+++ source/index.js
@@ -1,17 +1,21 @@
 // Core
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 // Instruments
+import { store } from './init/store';
 import './theme/init';
 
 // App
 import App from './navigation/App';
 
 render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store = { store }>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('app')
 );
