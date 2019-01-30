3c9febc481d405c46f325389b5d3275d51920309
scann <apple.eater01@gmail.com>
2019-01-30 14:27:25 +0200

    root reducer and store initialized

---
+++ source/init/store.js
@@ -0,0 +1,24 @@
+//Core
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+
+//Reducer
+import { rootReducer } from './rootReducer';
+
+const logger = createLogger({
+    duration:  true,
+    collapsed: true,
+    colors:    {
+        title:     () => '#139BFE',
+        prevState: () => '#1C5FAF',
+        action:    () => '#149945',
+        nextState: () => '#A47104',
+        error:     () => '#FF0005',
+    },
+});
+
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devtools ? devtools : compose;
+const enhancedStore = composeEnhancers(applyMiddleware(logger));
+
+export const store = createStore(rootReducer, enhancedStore);
