18ad090aaee3e530a857b0d30e9db0ad7d5ca561
scann <apple.eater01@gmail.com>
2019-01-30 15:38:43 +0200

    fetchPostsAsync action uses thunk to fetch data from server and call fillPosts action

--- source/bus/posts/actions.js
+++ source/bus/posts/actions.js
@@ -1,6 +1,9 @@
 //Types
 import { FETCH_POSTS_ASYNC, FILL_POSTS } from './types';
 
+//Instruments
+import { api } from '../../REST';
+
 export const fillPosts = (posts) => {
     return {
         type:    FILL_POSTS,
@@ -8,8 +11,14 @@ export const fillPosts = (posts) => {
     };
 };
 
-export const fetchPostsAsync = () => {
-    return {
+export const fetchPostsAsync = () => async (dispatch) => {
+    dispatch({
         type: FETCH_POSTS_ASYNC,
-    };
+    });
+
+    const response = await api.posts.fetch();
+    const result = await response.json();
+
+    dispatch(fillPosts(result.data));
+
 };
