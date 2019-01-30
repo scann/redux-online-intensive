d2f0433e2fc764a71e6e3b39567a6a88c3958d16
scann <apple.eater01@gmail.com>
2019-01-30 16:06:23 +0200

    Store refactoring - use enhanced Store from middleware

--- source/init/store.js
+++ source/init/store.js
@@ -1,32 +1,10 @@
 //Core
-import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 
 //Reducer
 import { rootReducer } from './rootReducer';
 
-const logger = createLogger({
-    duration:  true,
-    collapsed: true,
-    colors:    {
-        title:     () => '#139BFE',
-        prevState: () => '#1C5FAF',
-        action:    () => '#149945',
-        nextState: () => '#A47104',
-        error:     () => '#FF0005',
-    },
-});
-
-const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const composeEnhancers = __DEV__ && devtools ? devtools : compose;
-
-const middleware = [thunk];
-
-if (__DEV__) {
-    middleware.push(logger);
-}
-
-const enhancedStore = composeEnhancers(applyMiddleware(...middleware));
+//Enhancer
+import { enhancedStore } from './middleware/core';
 
 export const store = createStore(rootReducer, enhancedStore);
