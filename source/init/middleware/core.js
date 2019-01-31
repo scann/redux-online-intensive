35ea0687970e10809c60b21db50b4bc5d771ef5f
scann <apple.eater01@gmail.com>
2019-01-31 21:12:34 +0200

    Redux saga connected to middleware

--- source/init/middleware/core.js
+++ source/init/middleware/core.js
@@ -4,6 +4,7 @@ import { applyMiddleware, compose } from 'redux';
 //Middleware
 import { createLogger } from 'redux-logger';
 import { customThunk } from './custom';
+import createSagaMiddleware from 'redux-saga';
 
 const logger = createLogger({
     duration:  true,
@@ -16,11 +17,11 @@ const logger = createLogger({
         error:     () => '#FF0005',
     },
 });
-
+const sagaMiddleware = createSagaMiddleware();
 const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const composeEnhancers = __DEV__ && devtools ? devtools : compose;
 
-const middleware = [customThunk];
+const middleware = [sagaMiddleware, customThunk];
 
 if (__DEV__) {
     middleware.push(logger);
@@ -28,4 +29,4 @@ if (__DEV__) {
 
 const enhancedStore = composeEnhancers(applyMiddleware(...middleware));
 
-export { enhancedStore };
+export { enhancedStore, sagaMiddleware };
