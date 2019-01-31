36d8f1606b7c73b4c0dfd91243028dcb6a8f831f
scann <apple.eater01@gmail.com>
2019-01-31 21:14:10 +0200

    Refactoring - types as object

--- source/bus/posts/types.js
+++ source/bus/posts/types.js
@@ -1,6 +1,8 @@
-//Sync
-export const FILL_POSTS = 'FILL_POSTS';
-export const CREATE_POST = 'CREATE_POST';
-//Async
-export const FETCH_POSTS_ASYNC = 'FETCH_POSTS_ASYNC';
-export const CREATE_POST_ASYNC = 'CREATE_POST_ASYNC';
+export const types = {
+    //Sync
+    FILL_POSTS:        'FILL_POSTS',
+    CREATE_POST:       'CREATE_POST',
+    //Async
+    FETCH_POSTS_ASYNC: 'FETCH_POSTS_ASYNC',
+    CREATE_POST_ASYNC: 'CREATE_POST_ASYNC',
+};
