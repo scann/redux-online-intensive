59bf1c0647b2f3c719b2858e7475e4d705df8297
scann <apple.eater01@gmail.com>
2019-02-01 13:43:32 +0200

    Hometask - watchFetchPosts added to watcher saga

--- source/bus/posts/saga/watchers.js
+++ source/bus/posts/saga/watchers.js
@@ -5,12 +5,17 @@ import { takeEvery, all, call } from 'redux-saga/effects';
 import { types } from '../types';
 
 //Workers
-import { createPost } from './workers';
+import { createPost, fetchPosts } from './workers';
 
 export function* watchCreatePost () {
     yield takeEvery(types.CREATE_POST_ASYNC, createPost);
 }
 
-export function* watchPosts () {
-    yield all([call(watchCreatePost)]);
+export function* watchFetchPosts () {
+    yield takeEvery(types.FETCH_POSTS_ASYNC, fetchPosts);
 }
+
+export function* watchPosts () {
+    yield all([call(watchCreatePost, watchFetchPosts)]);
+}
+
