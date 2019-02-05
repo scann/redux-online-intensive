89dc7e3435bc9c205df31df48d83217555982837
scann <apple.eater01@gmail.com>
2019-02-05 16:14:05 +0200

    Auth domain - two types/actions - authenticate & signup

---
+++ source/bus/auth/actions.js
@@ -0,0 +1,19 @@
+//Types
+import { types } from './types';
+
+export const authAction = {
+    //Sync
+    authenticate: () => {
+        return {
+            type: types.AUTHENTICATE,
+        };
+    },
+
+    //Async
+    signupAsync: (userData) => {
+        return {
+            type:    types.SIGNUP_ASYNC,
+            payload: userData,
+        };
+    },
+};
