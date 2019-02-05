fae7b34b56e9723b9baaaab1819c9ca7ff11c571
scann <apple.eater01@gmail.com>
2019-02-05 18:31:09 +0200

    Signup worker performs request to server with "signup" API method

--- source/bus/auth/saga/workers/signup.js
+++ source/bus/auth/saga/workers/signup.js
@@ -1,6 +1,5 @@
 //Core
 import { put, apply } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
 
 //Instruments
 import { api } from '../../../../REST';
@@ -10,15 +9,17 @@ import { uiActions } from '../../../ui/actions';
 export function* signup ({ payload: userInfo }) {
     try {
         yield put(uiActions.startFetching());
-        console.log('userInfo: ', userInfo);
-        yield delay(2000);
-        yield put(authActions.authenticate());
-        //const response = yield apply(api, api.posts.fetch);
-        //const { data: posts, message } = yield apply(response, response.json);
 
-        //if (response.status !== 200) {
-        //    throw new Error(message);
-        //}
+        const response = yield apply(api, api.auth.signup, [userInfo]);
+        const { data: profile, message } = yield apply(response, response.json);
+
+        if (response.status !== 200) {
+            throw new Error(message);
+        }
+
+        console.log('--profile: ', profile);
+
+        yield put(authActions.authenticate());
     } catch (error) {
         yield put(uiActions.emitError(error, 'signup worker'));
     } finally {
