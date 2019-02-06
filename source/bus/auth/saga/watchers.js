307dccf93609803bd4e5e3afae1e985adc638b3b
scann <apple.eater01@gmail.com>
2019-02-06 16:17:30 +0200

    New watchInitialize watcher saga

--- source/bus/auth/saga/watchers.js
+++ source/bus/auth/saga/watchers.js
@@ -5,7 +5,7 @@ import { takeEvery, all, call } from 'redux-saga/effects';
 import { types } from '../types';
 
 //Workers
-import { login, signup, authenticate } from './workers';
+import { login, signup, authenticate,initialize } from './workers';
 
 function* watchSignup () {
     yield takeEvery(types.SIGNUP_ASYNC, signup);
@@ -17,7 +17,10 @@ function* watchLogin () {
 function* watchAuthenticate () {
     yield takeEvery(types.AUTHENTICATE_ASYNC, authenticate);
 }
+function* watchInitialize () {
+    yield takeEvery(types.INITIALIZE_ASYNC, initialize);
+}
 
 export function* watchAuth () {
-    yield all([call(watchSignup), call(watchLogin), call(watchAuthenticate)]);
+    yield all([call(watchSignup), call(watchLogin), call(watchAuthenticate), call(watchInitialize)]);
 }
