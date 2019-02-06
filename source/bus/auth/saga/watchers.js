da14b489bfe04fd34745030215cdbcbc3820c2de
scann <apple.eater01@gmail.com>
2019-02-06 14:07:21 +0200

    Hometask - new watchLogin watcher

--- source/bus/auth/saga/watchers.js
+++ source/bus/auth/saga/watchers.js
@@ -5,12 +5,16 @@ import { takeEvery, all, call } from 'redux-saga/effects';
 import { types } from '../types';
 
 //Workers
-import { signup } from './workers';
+import { login, signup } from './workers';
 
 export function* watchSignup () {
     yield takeEvery(types.SIGNUP_ASYNC, signup);
 }
 
+export function* watchLogin () {
+    yield takeEvery(types.LOGIN_ASYNC, login);
+}
+
 export function* watchAuth () {
-    yield all([call(watchSignup)]);
+    yield all([call(watchSignup), call(watchLogin)]);
 }
