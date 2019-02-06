3969478f1af6fbdfc2cf57d982022c9c55df7177
scann <apple.eater01@gmail.com>
2019-02-06 15:49:23 +0200

    App component connected to Redux

--- source/navigation/App.js
+++ source/navigation/App.js
@@ -8,16 +8,26 @@ import { withRouter } from 'react-router-dom';
 import Private from './Private';
 import Public from './Public';
 
+//Actions
+import { authActions } from '../bus/auth/actions';
+
 const mapStateToProps = (state) => {
     return {
         isAuthenticated: state.auth.get('isAuthenticated'),
     };
 };
 
+const mapDispatchToProps = {
+    authenticateAsync: authActions.authenticateAsync,
+};
+
 @hot(module)
 @withRouter
-@connect(mapStateToProps)
+@connect(mapStateToProps, mapDispatchToProps)
 export default class App extends Component {
+    componentDidMount () {
+        this.props.authenticateAsync();
+    }
     render () {
         const { isAuthenticated } = this.props;
 
