b7a7c02beb1219795686c69c8723112bb3ea5012
scann <apple.eater01@gmail.com>
2019-02-07 12:04:08 +0200

    Hometask - new API method - remove

--- source/REST/api.js
+++ source/REST/api.js
@@ -61,5 +61,13 @@ export const api = {
                 body: JSON.stringify({ comment }),
             });
         },
+        remove (postId) {
+            return fetch(`${MAIN_URL}/feed/${postId}`, {
+                method:  'DELETE',
+                headers: {
+                    Authorization: this.token,
+                },
+            });
+        },
     },
 };
