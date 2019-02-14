37e39918763e0d6fc496b1067800bdc5244f6fb1
scann <apple.eater01@gmail.com>
2019-02-14 15:12:18 +0200

    Socket connect/disconnect actions

---
+++ source/bus/socket/actions.js
@@ -0,0 +1,14 @@
+//Actions
+import { socket } from '../../init/socket';
+import { uiActions } from '../ui/actions';
+
+export const socketActions = {
+    listenConnection: () => (dispatch) => {
+        socket.on('connect', () => {
+            dispatch(uiActions.setOnlineState());
+        });
+        socket.on('disconnect', () => {
+            dispatch(uiActions.setOfflineState());
+        });
+    },
+};
