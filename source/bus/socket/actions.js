986cc5eddbccb1594116b710dfe07f51d05d1444
scann <apple.eater01@gmail.com>
2019-02-14 16:27:08 +0200

    "Like" socket event

--- source/bus/socket/actions.js
+++ source/bus/socket/actions.js
@@ -12,11 +12,25 @@ export const socketActions = {
             dispatch(uiActions.setOfflineState());
         });
     },
-    listenPosts: () => (dispatch) => {
+    listenPosts: () => (dispatch, getState) => {
         socket.on('create', (event) => {
             const { data: post } = JSON.parse(event);
 
             dispatch(postsActions.createPost(post));
+        });
+        socket.on('like', (event) => {
+            const { data, meta } = JSON.parse(event);
+
+            if (meta.action === 'like') {
+                const liker = getState().users.find((user) => user.get('id') === data.userId).delete('avatar');
+
+                dispatch(postsActions.likePost({
+                    postId: data.postId,
+                    liker,
+                }));
+            } else {
+                dispatch(postsActions.unlikePost(data));
+            }
         });
     },
 };
