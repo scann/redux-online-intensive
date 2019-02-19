6f497e366dcc13f7c5f5b3fd7fa5676cfbd9a9d2
scann <apple.eater01@gmail.com>
2019-02-19 15:03:56 +0200

    Profile form - clear userdata after logout

--- source/bus/auth/saga/workers/logout.js
+++ source/bus/auth/saga/workers/logout.js
@@ -1,6 +1,7 @@
 //Core
 import { put, apply } from 'redux-saga/effects';
 import { replace } from 'react-router-redux';
+import { actions } from 'react-redux-form';
 
 //Instruments
 import { api } from '../../../../REST';
@@ -30,6 +31,7 @@ export function* logout () {
         yield put(profileActions.clearProfile());
         yield put(postsActions.clearPosts());
         yield put(uiActions.stopFetching());
+        yield put(actions.reset('forms.user'));
         yield put(authActions.logout());
         yield put(replace(book.login));
     }
