e178a4530a03bd0f1d99910dfb3f5364ee071207
scann <apple.eater01@gmail.com>
2019-02-20 12:58:56 +0200

    Hometask - new feature updatePassword - worker, watcher and index update

--- source/bus/profile/saga/watchers.js
+++ source/bus/profile/saga/watchers.js
@@ -5,7 +5,7 @@ import { takeEvery, all, call } from 'redux-saga/effects';
 import { types } from '../types';
 
 //Workers
-import { updateName, updateAvatar } from './workers';
+import { updateName, updateAvatar, updatePassword } from './workers';
 
 export function* watchUpdateName () {
     yield takeEvery(types.UPDATE_NAME_ASYNC, updateName);
@@ -14,6 +14,11 @@ export function* watchUpdateName () {
 export function* watchUpdateAvatar () {
     yield takeEvery(types.UPDATE_AVATAR_ASYNC, updateAvatar);
 }
+
+export function* watchUpdatePassword () {
+    yield takeEvery(types.UPDATE_PASSWORD_ASYNC, updatePassword);
+}
+
 export function* watchProfile () {
-    yield all([call(watchUpdateName), call(watchUpdateAvatar)]);
+    yield all([call(watchUpdateName), call(watchUpdateAvatar), call(watchUpdatePassword)]);
 }
