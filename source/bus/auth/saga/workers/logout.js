342da3f13ea9bb8a88cecd20520c9ed18ad48446
scann <apple.eater01@gmail.com>
2019-02-23 20:49:19 +0200

    Clear users on logout

--- source/bus/auth/saga/workers/logout.js
+++ source/bus/auth/saga/workers/logout.js
@@ -9,6 +9,7 @@ import { authActions } from '../../../auth/actions';
 import { uiActions } from '../../../ui/actions';
 import { profileActions } from '../../../profile/actions';
 import { postsActions } from '../../../posts/actions';
+import { usersActions } from '../../../users/actions';
 import { book } from '../../../../navigation/book';
 
 export function* logout () {
@@ -30,6 +31,7 @@ export function* logout () {
         yield apply(localStorage, localStorage.removeItem, ['remember']);
         yield put(profileActions.clearProfile());
         yield put(postsActions.clearPosts());
+        yield put(usersActions.clearUsers());
         yield put(uiActions.stopFetching());
         yield put(actions.reset('forms.user'));
         yield put(authActions.logout());
