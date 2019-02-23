8006049b973cec59ea455923cf63719a68c49c96
scann <apple.eater01@gmail.com>
2019-02-23 22:07:05 +0200

    Hometask - tests for users reducer

---
+++ source/bus/users/__tests__/reducer.js
@@ -0,0 +1,24 @@
+//Core
+import { List, fromJS } from 'immutable';
+
+//Reducer
+import { usersReducer } from '../reducer';
+
+//Actions
+import { usersActions } from '../actions';
+
+const initialState = List();
+
+describe('users reducer', () => {
+    test('should return initial state by default', () => {
+        expect(usersReducer(void 0, {})).toEqual(initialState);
+    });
+
+    test('should handle FILL_USERS action', () => {
+        expect(usersReducer(void 0, usersActions.fillUsers(__.usersList))).toEqual(fromJS(__.usersList));
+    });
+
+    test('should handle CLEAR_USERS action', () => {
+        expect(usersReducer(void 0, usersActions.clearUsers())).toEqual(initialState.clear());
+    });
+});
