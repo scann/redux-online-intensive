6e3d70ec3efe09b8ba7e69c04960ec44e4649a79
scann <apple.eater01@gmail.com>
2019-02-23 22:05:32 +0200

    Hometask - usersReducer added to store test

--- source/init/__tests__/store.js
+++ source/init/__tests__/store.js
@@ -7,6 +7,7 @@ import { postsReducer as posts } from '../../bus/posts/reducer';
 import { uiReducer as ui } from '../../bus/ui/reducer';
 import { authReducer as auth } from '../../bus/auth/reducer';
 import { profileReducer as profile } from '../../bus/profile/reducer';
+import { usersReducer as users } from '../../bus/users/reducer';
 import { formsReducer as forms } from '../../bus/forms/reducer';
 
 //Store
@@ -17,6 +18,7 @@ const referenceRootReducer = combineReducers({
     ui,
     posts,
     profile,
+    users,
     router,
     forms,
 });
