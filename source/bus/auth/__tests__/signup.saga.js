0d5682795a8b7842fd1a7fde13f3ecc7fe51d456
scann <apple.eater01@gmail.com>
2019-02-28 13:40:25 +0200

    Tests for signup saga - unsuccessful scenario with 400 error handling

--- source/bus/auth/__tests__/signup.saga.js
+++ source/bus/auth/__tests__/signup.saga.js
@@ -12,3 +12,36 @@ const signupAction = authActions.signupAsync(__.userProfile);
 
 const saga = cloneableGenerator(signup)(signupAction);
 let clone = null;
+
+describe('signup saga', () => {
+    describe('should pass until response received', () => {
+        test('should dispatch "startFetching" action', () => {
+            expect(saga.next().value).toEqual(put(uiActions.startFetching()));
+        });
+
+        test('should call a fetch request', () => {
+            expect(saga.next().value).toEqual(apply(api, api.auth.signup, [__.userProfile]));
+            clone = saga.clone();
+        });
+    });
+
+    describe('should handle a 400 status response', () => {
+        test('a fetch request should return 400 status response', () => {
+            expect(clone.next(__.fetchResponseFail400).value).toEqual(
+                apply(__.fetchResponseFail400, __.fetchResponseFail400.json)
+            );
+        });
+
+        test('should contain a response data object', () => {
+            expect(clone.next(__.responseDataFail).value).toEqual(put(uiActions.emitError(__.error, 'signup worker')));
+        });
+
+        test('should dispatch "stopFetching" action', () => {
+            expect(clone.next().value).toEqual(put(uiActions.stopFetching()));
+        });
+
+        test('should finish', () => {
+            expect(clone.next().done).toBe(true);
+        });
+    });
+});
